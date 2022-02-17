75162850664bd837417753610a7233511afd3fc1
AloProud <1055256192@qq.com>
2022-02-17 10:43:01 +0800

    exercise

---
+++ 01Beginning/Exercise/1_5.cpp
@@ -0,0 +1,6 @@
+/*
+File:1_5
+Author:AloProud
+Data: 2022年02月17日Thu 10:02:08
+Description:
+*/
---
+++ 01Beginning/comments.cpp
@@ -0,0 +1,28 @@
+/*
+ * File:comments
+ * Author:AloProud
+ * Data: 2022年02月17日Thu 10:20:30
+ * Description:
+ */
+#include <iostream>
+/*
+ *简单主函数：
+ *读取两个数，求它们的和
+ *
+ */
+#include <iostream>
+/*
+ *简单主函数
+ *读取两个数，求它们的和
+ *
+ */
+int main(void)
+{
+    //提示用户输入两个数
+    std::cout << "请输入两个数";
+    std::cout << std::endl;
+    int v1, v2; //
+    std::cin >> v1 >> v2;
+    std::cout << v1 << "和" << v2 << "的积为" << v1 * v2 << std::endl;
+    return 0;
+}
